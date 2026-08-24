11c46f7595074e2d03aa558eac752cceb6c9ea3e
My Gitlab <marionya@gmail.com>
2026-08-24 14:39:16 +0300

    Initial commit

---
+++ environment/dev/main.tf
@@ -0,0 +1,32 @@
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  cloud {
+    organization = "patonya"
+
+    workspaces {
+      name = "cloud-infrastructure"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket_prefix = "hcp-terraform-demo-"
+
+  tags = {
+    Name        = "hcp-terraform-demo"
+    Environment = "dev"
+  }
+}
